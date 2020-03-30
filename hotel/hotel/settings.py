0ca2e52391ba1ee73573ff36a056db58ffb92bc9
Ahmed <sharif.ahmed@dxc.com>
2020-03-30 12:35:41 +0100

    updating code

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- hotel/property/models.py
+++ hotel/property/models.py
@@ -11,6 +11,7 @@ class Property(models.Model):
     name = models.CharField(max_length=50)
     property_type = models.CharField(choices=property_type, max_length=10)
     price = models.PositiveIntegerField()
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
     area = models.DecimalField(decimal_places=2, max_digits=8)
     beds_number = models.PositiveIntegerField()
     baths_number = models.PositiveIntegerField()
@@ -20,3 +21,17 @@ class Property(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Property'
+        verbose_name_plural = 'Properties'
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
